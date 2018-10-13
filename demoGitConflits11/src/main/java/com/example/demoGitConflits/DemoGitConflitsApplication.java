366caf7c8ab77b013df90328febc50ad342e92dc
pkamesh <kameswarao.pesingi@gmail.com>
2018-10-13 16:10:03 +0530

    sdfs

---
+++ demoGitConflits11/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
@@ -0,0 +1,12 @@
+package com.example.demoGitConflits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitConflitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitConflitsApplication.class, args);
+	}
+}
