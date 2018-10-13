8b0e0312d6ad9ec909d97628d27cabb353d1fedd
pkamesh <kameswarao.pesingi@gmail.com>
2018-10-13 19:31:42 +0530

    ffffdd

--- demoGitConflits11/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
+++ demoGitConflits11/src/main/java/com/example/demoGitConflits/DemoGitConflitsApplication.java
@@ -8,11 +8,11 @@ public class DemoGitConflitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoGitConflitsApplication.class, args);
-		//js33331dfs
-		//sfsdfsdfs
-			//sdfsdf
-		//sdfs
-		//sdfdff
+		//js33331dfs222222222
+		//sfsdfsdfs2222222222
+			//sdfsdf22222222222
+		//sdfs222222222222
+		//sdfdff2222222222
 		//sdfs22222211uuuuu11111111112222333333df222sf
 		
 		
